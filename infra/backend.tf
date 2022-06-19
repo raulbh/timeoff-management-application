8e3fdc59383f979afa7d2d356104aad996913697
Raul Bautista <raulbh@raul-work.localdomain>
2022-06-18 23:51:25 -0500

    fixing backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 # To store tfstate on gcs just remove the comment and change PROJECT_ID with the project to use
 # Note that you will need to create the bucket first
-#terraform {
-    #backend "gcs" {
-        #bucket = "PROJECT_ID-tfstate"
-        #prefix = "infra"
-    #}
-#}
+terraform {
+    backend "gcs" {
+        bucket = "project-01-raulbh-tfstate"
+        prefix = "infra"
+    }
+}
